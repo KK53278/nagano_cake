e668fad33f7984d91940105ebde3316bd566c305
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-11-16 07:02:04 +0000

    create devise admin, public

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  
+  devise_for :customers, controllers: {
+    sessions: 'public/sessions',
+    passwords:     'public/passwords',
+    registrations: 'public/registrations',
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+  
+  namespace :admin do
+  get "top" => "homes#top", as: "top"
+  resources :items, except: [:destroy]
 end
+  
+end
+
