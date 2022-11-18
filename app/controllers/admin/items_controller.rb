2cbd5f3e4c2436fe5369f5077ae231aefa57c3ac
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-11-18 09:17:16 +0000

    edit items new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,16 @@
 class Admin::ItemsController < ApplicationController
   
+  
+  def new
+    @item = Item.new
+  end 
+  
   def index
   end 
   
-  def new
-  end 
-  
   def create
+    @item = Item.new(item_params)
+    @item.save ? (redirect_to admin_item_path(@item)) : (render :new)
   end 
   
   def show
@@ -16,6 +20,12 @@ class Admin::ItemsController < ApplicationController
   end 
   
   def update 
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :image, :price, :is_active)
   end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  has_one_attached :image
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  #指定された値と等しいか、それよりも大きくなければならないことを指定
+  validate :image_type
+
+def get_image(*size)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no-image.png')
+    image.attach(io: File.open(file_path), filename: 'no-image.png', content_type: 'image/png')
+　end
+　if !size.empty?
+    image.variant(resize: size)
+  else
+    image
+  end
+end 
+
 end
