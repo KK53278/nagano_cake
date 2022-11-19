447048536f72f210c6c8212317689bca20a70576
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-11-19 15:27:13 +0000

    create items index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,31 +1,35 @@
 class Admin::ItemsController < ApplicationController
-  
-  
+
+
   def new
     @item = Item.new
-  end 
-  
+  end
+
   def index
-  end 
-  
+    @items = Item.all
+  end
+
   def create
     @item = Item.new(item_params)
-    @item.save ? (redirect_to admin_item_path(@item)) : (render :new)
-  end 
-  
+    @item.save ? (redirect_to admin_items_path(@item)) : (render :new)
+  end
+
   def show
   end
-  
+
   def edit
-  end 
-  
-  def update 
   end
-  
+
+  def update
+  end
+
   private
-  
+
   def item_params
     params.require(:item).permit(:genre_id, :name, :introduction, :image, :price, :is_active)
   end
-  
+
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
 end
