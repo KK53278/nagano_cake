9050b4f949246c5ccd235220a8270e030f827814
EC2 Default User <110440486+KK53278@users.noreply.github.com>
2022-12-18 06:26:13 +0000

    fixed public signup & login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :admins, controllers: {
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
     sessions: 'admin/sessions',
   }
 
@@ -15,9 +15,8 @@ Rails.application.routes.draw do
   end
 end
 
-  devise_for :customers, controllers: {
+  devise_for :customers,skip: [:passwords], controllers: {
     sessions: 'public/sessions',
-    passwords:     'public/passwords',
     registrations: 'public/registrations',
   }
 
