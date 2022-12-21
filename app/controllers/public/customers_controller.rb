946df58b47087cb1b54a16cdebf7062de6974c4b
EC2 Default User <110440486+KK53278@users.noreply.github.com>
2022-12-21 04:07:01 +0000

    create public customer mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,7 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_current_customer
+
   def show
   end
 
@@ -6,12 +9,27 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    if @customer.update(customer_params)
+      redirect_to mypage_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdrew
+  end
+
+  private
+
+  def set_current_customer
+    @customer = current_customer
+  end
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number)
   end
 
 end
